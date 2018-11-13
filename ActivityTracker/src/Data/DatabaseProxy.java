d9edec23766f62ac242435e83a593612f1349f98
stevep1983 <swp147@gmail.com>
2018-11-13 11:53:54 -0330

    Worked on persistence

--- ActivityTracker/src/Data/DatabaseProxy.java
+++ ActivityTracker/src/Data/DatabaseProxy.java
@@ -4,37 +4,42 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.sql.*;
+
+import GUI.Panels.UserRegistrationPanel;
+
 /**
  * This object acts as a middle man between the GUI and the database
+ * 
  * @author k47swp
  *
  */
-public class DatabaseMediator {
+public class DatabaseProxy {
 
 	String databaseFileName;
 	Connection connection;
 	Statement statement;
 
 	/**
-	 * This is the default constructor. it simply initializes the class fields
-	 * to null values. 
+	 * This is the default constructor. it simply initializes the class fields to
+	 * null values.
 	 */
-	DatabaseMediator() {
+	DatabaseProxy() {
 		databaseFileName = null;
 		connection = null;
 		statement = null;
 	}
-	
+
 	/**
-	 * This constructor creates this object, and connects to a database
-	 * specified by a particular filename. This constructor is simply
-	 * an overloaded version of the default constructor.
-	 *  
+	 * This constructor creates this object, and connects to a database specified by
+	 * a particular filename. This constructor is simply an overloaded version of
+	 * the default constructor.
+	 * 
 	 * @param fname This is the name of the local file that has the database.
-	 *  
+	 * 
 	 */
-	DatabaseMediator(String fname) {
+	DatabaseProxy(String fname) {
 		this();
+		this.databaseFileName = fname;
 		connect(fname);
 	}
 
@@ -59,69 +64,90 @@ public class DatabaseMediator {
 	 * Closes the connection to the database.
 	 */
 	void close() {
-		
+
 		// If we aren't actually connected to a db, then nothing to do.
-		if(connection==null) { return; }
-		
+		if (connection == null) {
+			return;
+		}
+
 		// Close the connection and clean up reference
 		try {
 			connection.close();
-			connection=null;
+			connection = null;
 			System.out.println("Closed db");
 		} catch (SQLException e) {
-			
+
 			e.printStackTrace();
 			System.exit(0);
 		}
 	}
 
 	/**
-	 * Creates an example table, for reference
+	 * Creates a table
 	 */
-	void createExampleTable() {
+	void createTable(String query) {
 
 		// Make sure that the database is open
 		if (connection == null) {
 			System.out.println("Database is not opened");
 			return;
 		}
-		
-		// Create an example table
+
+		// Create a user table
 		try {
 			statement = connection.createStatement();
-			String query = "CREATE TABLE COMPANY "
-					+"(ID INT PRIMARY KEY     NOT NULL,"
-					+ " NAME           TEXT    NOT NULL, "
-					+ " AGE            INT     NOT NULL, "
-					+ " ADDRESS        CHAR(50), " + " SALARY         REAL)";
+			
 			statement.executeUpdate(query);
 			statement.close();
 			statement = null;
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 			System.exit(0);
 		}
-		
+
 		// Let the user know it happened
 		System.out.println("Table created successfully");
 	}
-
+	
+	// TODO: Change to a constant string
+	String userTableCreationString() {
+		return
+			"CREATE TABLE IF NOT EXISTS USER  ("
+			+ " firstName	TEXT NOT NULL, "
+			+ " lastName    TEXT NOT NULL,"
+			+ " username    TEXT NOT NULL,"
+			+ " password	TEXT NOT NULL,"
+			+ " image		BLOB" + ");";
+	}
+	
+	// TODO: Change to a constant string
+	String activityTableCreationString() {
+		return
+			"CREATE TABLE IF NOT EXISTS ACTIVITY  ("
+			+ " userId		INTEGER, "
+			+ " time		INTEGER,"
+			+ " distance	REAL,"
+			+ " altitude	REAL"
+			+ ");";
+	}
+	
 	/**
 	 * Main method, used for debugging, not intended to be used in production
 	 */
 	public static void main(String[] args) {
-		
+		System.out.println(Paths.get("test2.db"));
 		try {
 			Files.deleteIfExists(Paths.get("test2.db"));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		DatabaseMediator databaseMediator = new DatabaseMediator("test2.db");
-		databaseMediator.close();
-		databaseMediator.connect("test2.db");
-		databaseMediator.createExampleTable();
-		databaseMediator.close();
+		DatabaseProxy databaseProxy = new DatabaseProxy("test2.db");
+		databaseProxy.close();
+		databaseProxy.connect("test2.db");
+		databaseProxy.createTable(databaseProxy.userTableCreationString());
+		databaseProxy.createTable(databaseProxy.activityTableCreationString());
+		databaseProxy.close();
 	}
 }
--- ActivityTracker/src/Data/User.java
+++ ActivityTracker/src/Data/User.java
@@ -1,6 +1,11 @@
 package Data;
 
 import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.util.Base64;
+
+import javax.imageio.ImageIO;
 
 public class User implements DBHandler {
 
@@ -9,10 +14,10 @@ public class User implements DBHandler {
 	String firstName;
 	String lastName;
 	boolean isValid;
-	Image image;
-	DatabaseMediator m;
+	BufferedImage image;
+	DatabaseProxy m;
 	
-	User(DatabaseMediator m, String username, String password, String firstName, String lastName, Image image) {
+	User(DatabaseProxy m, String username, String password, String firstName, String lastName, Image image) {
 		this.m = m;
 		this.username = username;
 		this.password = password;
@@ -25,6 +30,9 @@ public class User implements DBHandler {
 	User(String username, String password) {
 		this.username = username;
 		this.password = password;
+		this.firstName = null;
+		this.lastName = null;
+		this.image = null;
 		isValid = this.validate();
 	}
 	
@@ -33,6 +41,38 @@ public class User implements DBHandler {
 	}
 	
 	public boolean validate() {
+//	http://www.sqlitetutorial.net/sqlite-select/
+//		SELECT DISTINCT column_list
+//		FROM table_list
+//		  JOIN table ON join_condition
+//		WHERE row_filter
+//		ORDER BY column
+//		LIMIT count OFFSET offset
+//		GROUP BY column
+//		HAVING group_filter;
+		
+		// CASE 1: If we are validating an existing user, the firstName will be null
+		if(this.firstName == null) {
+			String query = "SELECT rowid, * FROM USER "
+					+ "WHERE username = " + this.username + ", "
+					+ "WHERE password = " + this.password + ";";
+			// need a function to execute query and see if there is a matching row
+		}
+		
+		// CASE 2: We are creating a user
+		
+		// Ugly hack to store image into a string
+		// DOESNT WORK, FIX LATER
+		ByteArrayOutputStream os = new ByteArrayOutputStream();
+		ImageIO.write(image, "png", os);
+		String encodedImage = new String(Base64.getEncoder().encodeToString(os))
+		String query = "INSERT INTO USER VALUES("
+				+ this.firstName + ", "
+				+ this.lastName + ", "
+				+ this.username + ", "
+				+ this.password + ", "
+				+ encodedImage + ");";
+		
 		return false;
 	}
 
